0807ec5841c20fd7231abb00b830b57d01eae14b
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-24 15:36:02 -0400

    added more logic to input fields

--- src/Validator.js
+++ src/Validator.js
@@ -27,15 +27,25 @@ class Validator extends Component {
       confirmPassword: event.target.value
     })
   }
-  
+  handleSubmit(event){
+    event.preventDefault()
+    this.handleValidity()
+  }
+  handleValidity(event){
+    if(this.state.password === this.state.confirmPassword){
+      this.state({valid: true})
+    }else{
+      this.state({valid: false})
+    }
+  }
   render() {
     return (
       <div className="form">
         <h1>Sign Up</h1>
-        <input type="text" placeholder="email" />
-        <input type="password" placeholder="password" />
-        <input type="password" placeholder="confirm password" />
-        <input type="submit" value="Submit" />
+        <input onChange={(event) => this.handleEmail(event)} type="text" placeholder="email" />
+        <input onChange={(event) => this.handlePassword(event)} type="password" placeholder="password" />
+        <input onChange={(event) => onChange} type="password" placeholder="confirm password" />
+        <input type="submit" onClick={(event) => this.handleValidity(event)} value="Submit" />
       </div>
     );
   }
