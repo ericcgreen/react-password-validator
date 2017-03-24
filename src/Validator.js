b3caae3ec34acee926dcdda02f5bf06f58e01c33
Eric Christopher Green <VERDE611@GMAIL.COM>
2017-03-24 15:07:53 -0400

    saved logic for input fields

--- src/Validator.js
+++ src/Validator.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import "./Validator.css";
 
 class Validator extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      email: '',
+      password: '',
+      confirmPassword: '',
+      valid: true
+    }
+  }
+
+  handleEmail(event){
+    this.State({
+      email: event.target.value
+    })
+  }
+  handlePassword(event){
+    this.State({
+      password: event.target.value
+    })
+  }
+  handleConfirmPassword(event){
+    this.State({
+      confirmPassword: event.target.value
+    })
+  }
+  
   render() {
     return (
       <div className="form">
